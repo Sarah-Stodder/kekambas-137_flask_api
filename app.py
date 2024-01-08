b82570c7f68fd91943ed43aba6246c5dd1cbb8ca
Brian <bstanton773@gmail.com>
2024-01-08 15:03:49 -0600

    Basic setup of Flask app with one route

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    first_name = 'Brian'
+    last_name = 'Stanton'
+    return f'Hello World!! - From {first_name} {last_name}'
