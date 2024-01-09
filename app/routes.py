a6911da83f7b567963205f6a9efa9a83b012db2c
Brian <bstanton773@gmail.com>
2024-01-09 11:48:29 -0600

    Add a Create Post route

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import request
 from app import app
 from fake_data.posts import post_data
 
@@ -30,4 +31,37 @@ def get_post(post_id):
             return post
     # If we loop through all of the posts without returning, the post with that ID does not exist
     return {'error': f'Post with an ID of {post_id} does not exist'}, 404
+
+# Create new Post route
+@app.route('/posts', methods=['POST'])
+def create_post():
+    # Check to see that the request body is JSON
+    if not request.is_json:
+        return {'error': 'You content-type must be application/json'}, 400
+    # Get the data from the request body
+    data = request.json
+    # Validate the incoming data
+    required_fields = ['title', 'body']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
     
+    # Get data from the body
+    title = data.get('title')
+    body = data.get('body')
+
+    # Create a new post with data
+    new_post = {
+        "id": len(post_data) + 1,
+        "title": title,
+        "body": body,
+        "userId": 1,
+        "dateCreated": "2024-01-09T11:25:45",
+        "likes": 0
+    }
+    # Add the new post to the list of posts
+    post_data.append(new_post)
+    return new_post, 201
