cdacd6262d00897b364e57a91b4c9ae6a933a08b
Brian <bstanton773@gmail.com>
2024-01-10 12:11:55 -0600

    Add User Model methods for saving to database and hashing password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -10,3 +11,21 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        self.save()
+
+    def check_password(self, plain_text_password):
+        return check_password_hash(self.password, plain_text_password)
