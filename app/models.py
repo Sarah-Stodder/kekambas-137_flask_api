dcf0c6952a5e305f0f7d166c4ce73773253a8953
Brian <bstanton773@gmail.com>
2024-01-10 15:10:13 -0600

    Add post table with migrations

--- app/models.py
+++ app/models.py
@@ -38,3 +38,23 @@ class User(db.Model):
             "username": self.username,
             "email": self.email
         }
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # In SQL - user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Post {self.id}|{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
